770773b82eeac159eac3327096a71ec746eb9e9e
aliakseiazarau <aliaksei.azarau@startmatter.com>
2019-10-04 15:20:59 +0300

    Fix fixture for btn displaying test

--- test_items.py
+++ test_items.py
@@ -1,7 +1,5 @@
-
-
-def test_something(browser):
-    link = ("http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/")
+def test_button_displaying(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     cart_button = browser.find_elements_by_class_name("btn-add-to-basket")
     assert len(cart_button) > 0
